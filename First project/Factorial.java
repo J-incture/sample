2ca716af1dd405a14f12a1fe2871b1b648ed872d
vijay <vijay@LAPTOP-LN45122I>
2021-05-27 11:42:55 +0530

    initial commit

---
+++ First project/Factorial.java
@@ -0,0 +1,19 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 20;
+		int factorial = 1;
+		
+		int i=1;
+		while (i<=n)
+		{
+			factorial = factorial * i;
+			i++;
+		}
+		
+		System.out.println("The factorial of " + n + "is" + factorial);
+	}
+
+}
